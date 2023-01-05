eb55a7e2fce6ad71e397e62fd37ebff825185bc6
Nycolas R. Alves <nycolasramon3@gmail.com>
2023-01-05 00:57:06 -0300

    Criado componente jogo-da-velha

--- src/app/jogo-da-velha/jogo-da-velha.module.ts
+++ src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { JogoDaVelhaComponent } from './jogo-da-velha.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    JogoDaVelhaComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    JogoDaVelhaComponent // export deve ser feito para renderização do componente, já que não há roteamento
   ]
 })
 export class JogoDaVelhaModule { }
