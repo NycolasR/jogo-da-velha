c682d3fc22bf4e1e70d5eafccc4af9981b7e9dd7
Nycolas R. Alves <nycolasramon3@gmail.com>
2023-01-11 00:14:26 -0300

    Implementando a tela do tabuleiro

--- src/app/jogo-da-velha/jogo-da-velha.component.ts
+++ src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -48,4 +48,48 @@ export class JogoDaVelhaComponent implements OnInit {
   iniciarJogo(): void {
     return this.jogoDaVelhaService.iniciarJogo();
   }
+
+  /**
+   * Realiza uma jogada ao clicar num local do tabuleiro.
+   *
+   * @param number posX
+   * @param number posY
+   * @returns void
+   */
+  jogar(posX: number, posY: number): void {
+    this.jogoDaVelhaService.jogar(posX, posY);
+  }
+
+  /**
+   * Retorna se a peça X deve ser exibida para a coordenada informada
+   *
+   * @param number posX
+   * @param number posY
+   * @returns boolean
+   */
+  exibirX(posX: number, posY: number): boolean {
+    return this.jogoDaVelhaService.exibirX(posX, posY);
+  }
+
+  /**
+   * Retorna se a peça O deve ser exibida para a coordenada informada
+   *
+   * @param number posX
+   * @param number posY
+   * @returns boolean
+   */
+  exibirO(posX: number, posY: number): boolean {
+    return this.jogoDaVelhaService.exibirO(posX, posY);
+  }
+
+  /**
+   * Retorna se a marcação de vitória deve ser exibida para a coordenada informada
+   *
+   * @param number posX
+   * @param number posY
+   * @returns boolean
+   */
+  exibirVitoria(posX: number, posY: number): boolean {
+    return this.jogoDaVelhaService.exibirVitoria(posX, posY);
+  }
 }
--- src/app/jogo-da-velha/shared/jogo-da-velha-o.service.ts
+++ src/app/jogo-da-velha/shared/jogo-da-velha-o.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class JogoDaVelhaService {
 
   private readonly TAM_TAB: number = 3;
@@ -19,12 +17,12 @@ export class JogoDaVelhaService {
   private _showTabuleiro: boolean;
   private _showFinal: boolean;
 
-  constructor() {}
+  constructor() { }
 
   /**
    * Inicializa o jogo. Define exibição da tela inicial.
    *
-   * @returns void
+   * @return void
    */
   inicializar(): void {
     this._showInicio = true;
@@ -37,9 +35,10 @@ export class JogoDaVelhaService {
   }
 
   /**
-   * Inicializa o tabuleiro do jogo com vazio para todas as posições.
+   * Inicializa o tabuleiro do jogo com vazio para todas 
+   * as posições.
    *
-   * @returns void
+   * @return void
    */
   inicializarTabuleiro(): void {
     this.tabuleiro = [this.TAM_TAB];
@@ -49,18 +48,18 @@ export class JogoDaVelhaService {
   }
 
   /**
-   * Retorna se a tela de inicio deve ser exibida.
-   *
-   * @returns boolean
+   * Retorna se a tela de início deve ser exibida.
+   * 
+   * @return boolean
    */
   get showInicio(): boolean {
     return this._showInicio;
   }
 
   /**
-   * Retorna se a tela de tabuleiro deve ser exibida.
-   *
-   * @returns boolean
+   * Retorna se o tabuleiro deve ser exibido.
+   * 
+   * @return boolean
    */
   get showTabuleiro(): boolean {
     return this._showTabuleiro;
@@ -68,17 +67,17 @@ export class JogoDaVelhaService {
 
   /**
    * Retorna se a tela de fim de jogo deve ser exibida.
-   *
-   * @returns boolean
+   * 
+   * @return boolean
    */
   get showFinal(): boolean {
     return this._showFinal;
   }
 
   /**
-   * Retorna o número do jogador a jogar
-   *
-   * @returns number
+   * Retorna o número do jogador a jogar.
+   * 
+   * @return number
    */
   get jogador(): number {
     return this._jogador;
@@ -87,7 +86,7 @@ export class JogoDaVelhaService {
   /**
    * Exibe o tabuleiro.
    *
-   * @returns void
+   * @return void
    */
   iniciarJogo(): void {
     this._showInicio = false;
@@ -99,18 +98,19 @@ export class JogoDaVelhaService {
    *
    * @param number posX
    * @param number posY
-   * @returns void
+   * @return void
    */
   jogar(posX: number, posY: number): void {
-    console.log(`posX: ${posX} || posY: ${posY}`);
     // jogada inválida
-    if (this.tabuleiro[posX][posY] !== this.VAZIO || this.vitoria) {
+    if (this.tabuleiro[posX][posY] !== this.VAZIO || 
+      this.vitoria) {
       return;
     }
 
     this.tabuleiro[posX][posY] = this._jogador;
     this.numMovimentos++;
-    this.vitoria = this.fimJogo(posX, posY, this.tabuleiro, this._jogador);
+    this.vitoria = this.fimJogo(posX, posY, 
+      this.tabuleiro, this._jogador);
     this._jogador = (this._jogador === this.X) ? this.O : this.X;
 
     if (!this.vitoria && this.numMovimentos < 9) {
@@ -129,7 +129,17 @@ export class JogoDaVelhaService {
     }
   }
 
-  fimJogo(linha: number, coluna: number, tabuleiro: any, jogador: number) {
+  /**
+   * Verifica e retorna se o jogo terminou.
+   *
+   * @param number linha
+   * @param number coluna
+   * @param any tabuleiro
+   * @param number jogador
+   * @return array
+   */
+  fimJogo(linha: number, coluna: number, 
+      tabuleiro: any, jogador: number) {
     let fim: any = false;
 
     // valida a linha
@@ -164,8 +174,8 @@ export class JogoDaVelhaService {
 
   /**
    * Lógica para simular jogada do computador em modo aleatório.
-   * 
-   * @returns void
+   *
+   * @return void
    */
   cpuJogar(): void {
     // verifica jogada de vitória
@@ -179,81 +189,78 @@ export class JogoDaVelhaService {
     if (jogada.length <= 0) {
       // joga aleatório
       let jogadas: any = [];
-
-      for (let i = 0; i < this.TAM_TAB; i++) {
-        for (let j = 0; j < this.TAM_TAB; j++) {
+      for (let i=0; i<this.TAM_TAB; i++) {
+        for (let j=0; j<this.TAM_TAB; j++) {
           if (this.tabuleiro[i][j] === this.VAZIO) {
             jogadas.push([i, j]);
           }
         }
       }
-
       let k = Math.floor((Math.random() * (jogadas.length - 1)));
       jogada = [jogadas[k][0], jogadas[k][1]];
     }
 
     this.tabuleiro[jogada[0]][jogada[1]] = this._jogador;
     this.numMovimentos++;
-    this.vitoria = this.fimJogo(jogada[0], jogada[1], this.tabuleiro, this._jogador);
+    this.vitoria = this.fimJogo(jogada[0], jogada[1],
+        this.tabuleiro, this._jogador);
     this._jogador = (this._jogador === this.X) ? this.O : this.X;
   }
 
   /**
-   * Obtém uma jogada válida para vitória de um jogador
-   * 
-   * @param number jogador 
-   * @returns number[]
+   * Obtém uma jogada válida para vitória de um jogador.
+   *
+   * @param number jogador
+   * @return nomber[]
    */
   obterJogada(jogador: number): number[] {
     let tab = this.tabuleiro;
-
     for (let lin = 0; lin < this.TAM_TAB; lin++) {
       for (let col = 0; col < this.TAM_TAB; col++) {
         if (tab[lin][col] !== this.VAZIO) {
           continue;
         }
-
         tab[lin][col] = jogador;
-
         if (this.fimJogo(lin, col, tab, jogador)) {
           return [lin, col];
         }
-
         tab[lin][col] = this.VAZIO;
       }
     }
-
     return [];
   }
 
   /**
-   * Retorna se a peça X deve ser exibida para a coordenada informada.
-   * 
-   * @param number posX 
-   * @param number posY 
-   * @returns boolean
+   * Retorna se a peça X deve ser exibida para a 
+   * coordena informada.
+   *
+   * @param number posX
+   * @param number posY
+   * @return boolean
    */
   exibirX(posX: number, posY: number): boolean {
     return this.tabuleiro[posX][posY] === this.X;
   }
 
   /**
-   * Retorna se a peça O deve ser exibida para a coordenada informada.
-   * 
-   * @param number posX 
-   * @param number posY 
-   * @returns boolean
+   * Retorna se a peça O deve ser exibida para a 
+   * coordena informada.
+   *
+   * @param number posX
+   * @param number posY
+   * @return boolean
    */
   exibirO(posX: number, posY: number): boolean {
     return this.tabuleiro[posX][posY] === this.O;
   }
 
   /**
-   * Retorna se a marcação de vitória deve ser exibida para a coordenada informada.
-   * 
-   * @param number posX 
+   * Retorna se a marcação de vitória deve ser exibida para a 
+   * coordena informada.
+   *
+   * @param number posX
    * @param number posY
-   * @returns boolean
+   * @return boolean
    */
   exibirVitoria(posX: number, posY: number): boolean {
     let exibirVitoria: boolean = false;
@@ -273,9 +280,9 @@ export class JogoDaVelhaService {
   }
 
   /**
-   * Inicializa um novo jogo, assim como exibe o tabuleiro
-   * 
-   * @returns void
+   * Inicializa um novo jogo, assim como exibe o tabuleiro.
+   *
+   * @return void
    */
   novoJogo(): void {
     this.inicializar();
@@ -283,4 +290,5 @@ export class JogoDaVelhaService {
     this._showInicio = false;
     this._showTabuleiro = true;
   }
+
 }
